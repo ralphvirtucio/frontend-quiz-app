ee7c8815e3a7984d9a691ce0c02062d8549db237
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-08-26 23:25:53 +0800

    WIP: Mobile Question Light

--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 
-const toggle = document.querySelector('[aria-pressed]');
+// const toggle = document.querySelector('[aria-pressed]');
 
 
-const handleToggleTheme = (e) => {
-  const isPress = e.target.getAttribute('aria-pressed') === 'true';
-  e.target.setAttribute('aria-pressed', String(!isPress));
+// const handleToggleTheme = (e) => {
+//   const isPress = e.target.getAttribute('aria-pressed') === 'true';
+//   e.target.setAttribute('aria-pressed', String(!isPress));
 
-  if(!isPress) {
-    e.target.dataset.theme = 'light'
-  } else {
-    e.target.dataset.theme = 'dark'
+//   if(!isPress) {
+//     e.target.dataset.theme = 'light'
+//   } else {
+//     e.target.dataset.theme = 'dark'
+//   }
+// }
+
+
+// // Event Listeners
+// toggle.addEventListener("click", handleToggleTheme)
+const subButtons = document.querySelectorAll(".btn__subject")
+
+
+subButtons.forEach(btn => {
+  btn.addEventListener('click', async (e) => {
+    const value = e.target.textContent.trim()
+    
+    try {
+      const res = fetchData(value)
+
+      res.then(data => {
+        console.log(data, 'button ' + value)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  })
+  
+})
+
+const fetchData = async (query) => {
+  try {
+    const res = await fetch('data.json');
+
+    if(!res.ok) {
+      throw new Error("Something went wrong: " + res.status)
+    }
+
+    const data = await res.json()
+
+    const quiz = data.quizzes.filter(quiz => quiz.title === query)
+
+    return quiz
+    
+  } catch (error) {
+    console.error(error.message)
   }
 }
 
-
-// Event Listeners
-toggle.addEventListener("click", handleToggleTheme)
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData()
+})
