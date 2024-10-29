231768531a73ffbaff9ac8cb683310ffdca861c1
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-10-30 00:01:54 +0800

    Features: All functionality, except play-again button

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const subjectForm = document.querySelector('#subject-menu')
+const themeSwitcher = document.querySelector('#themeSwitcher')
 const subjectButtons = subjectForm.querySelectorAll('.subject')
 
+const body = document.body
+
+const savedTheme = localStorage.getItem("theme") || "light"
+body.classList.add(`${savedTheme}-theme`);
+themeSwitcher.setAttribute("aria-label", `Switch to ${savedTheme === "light" ? "dark" : "light"} theme`)
+
+themeSwitcher.addEventListener("click", () => {
+  const isDark = body.classList.toggle('dark-theme')
+  body.classList.toggle('light-theme', !isDark)
+
+  const newTheme = isDark ? "dark" : "light";
+  localStorage.setItem("theme", newTheme)
+  themeSwitcher.setAttribute("aria-label", `Switch to ${newTheme === "light" ? "dark" : "light"} theme`);
+})
 
 const sessionStoreQuiz = (quiz, subject) => {
   const selectedQuiz = quiz.find(data => data.title.toLowerCase() === subject)
